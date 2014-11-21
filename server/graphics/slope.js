b30c7ef8f6e36ad4f90cc1f4dde89269c904ef9e
tomgp <tom.g.pearson@gmail.com>
2014-11-21 17:28:30 +0000

    Adding slope chart stuff

--- server/graphics/slope.js
+++ server/graphics/slope.js
@@ -2,14 +2,17 @@
 var d3 = require('d3');
 
 module.exports = function (selection, data, options){
+
 	selection
 		.append('svg').attr({
 			xmlns:'http://www.w3.org/2000/svg',
 			width:500,
 			height:500
-		}).selectAll('text').data(data).enter()
-			.append('g').attr('transform','translate(20,20)')
+		}).selectAll('text').data(data.parties).enter()
+			.append('g').attr('transform',function(d,i){ return 'translate(20,'+ (20*i +20) +')'})
 			.append('text')
-				.text(function(d){ return d; })
+				.text(function(d){
+					return d.name;
+				})
 				.attr({fill:'#000'});
 }
