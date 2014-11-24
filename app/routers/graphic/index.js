73b2eb10db0074091ef727e95e338233e4e5a631
tomgp <tom.g.pearson@gmail.com>
2014-11-24 12:23:00 +0000

    adding party lookup stuff and fixing slope colouring

--- app/routers/graphic/index.js
+++ app/routers/graphic/index.js
@@ -1,6 +1,7 @@
 var Router = require('koa-router');
 var koa = require('koa');
 var svg = require('../../graphics/svg');
+var parties = require('../../data/parties');
 var slopeLayout = require('../../graphics/slope-layout')();
 
 //TODO, make these set-able (explicitly via the route or a series of pre defined configs (Large, medium, XL etc)...?)
@@ -18,6 +19,10 @@ slopeLayout.end(function(d){
 
 slopeLayout.domain( [0,100] );
 slopeLayout.range( [200,0] );
+slopeLayout.attr({
+	'fill':function(d){ console.log(d.name, '---', parties); return parties[d.name].primarycolor; },
+	'stroke':function(d){ return parties[d.name].secondarycolor; }
+})
 
 module.exports = function() {
   var router = koa();
@@ -26,15 +31,17 @@ module.exports = function() {
   router.get('slope', '/slope/:constituency', function* (next){
   	this.type = 'image/svg+xml';
   	var constituencyResults = getResultsData(this.params.constituency);
+
   	var plotData = {
   		name: constituencyResults.name,
   		label:function(d){ return d.data.party; },
-  		color:function(d){ return colorScale(d.data.party); },
   		slopes: slopeLayout( constituencyResults.parties ),
   		width: width,
   		height: height,
   		margin: margin
   	};
+  	console.log(plotData.slopes);
+
   	this.body = yield svg('slope', plotData);
   	yield next;
   });
@@ -45,9 +52,7 @@ module.exports = function() {
 
 //TODO factor out colour scales
 var d3 = require('d3');
-var colorScale = d3.scale.ordinal()
-	.range(['#F00','#00F','#FA0','#A0F'])
-	.domain(['Labour','Conservative','Liberal Democrat','UKIP']);
+
 
 //TODO replace this dummy data thing
 function getResultsData(constituencyID){
@@ -62,7 +67,7 @@ function getResultsData(constituencyID){
 			{name:'Labour', pct2005:'46', pct2010:'37'},
 			{name:'Conservative', pct2005:'43', pct2010:'51'},
 			{name:'Liberal Democrat', pct2005:'7', pct2010:'8'},
-			{name:'UKIP', pct2005:'0', pct2010:'4'}
+			{name:'UK Independence Party', pct2005:'0', pct2010:'4'}
 		]
 	}
 }
