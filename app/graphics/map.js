f23b570cb807fa9581d984995dd9d47492700dae
tomgp <tom.g.pearson@gmail.com>
2014-11-27 12:17:18 +0000

    drawing a constituency

--- app/graphics/map.js
+++ app/graphics/map.js
@@ -2,7 +2,35 @@
 
 var d3 = require('d3');
 
+
+//TODO .. fix this projection so it's not centered on Wales
+
+
+
+
 module.exports = function (selection, data, options){
 	console.log('a map');
 	console.log('map data ', data.geoJSON);
+
+	var projection = d3.geo.albers()
+	    .center([-3.5, 52.54])
+	    .rotate([0, 0])
+	    .parallels([50, 60])
+	    .scale(6000)
+	    .translate([data.width / 2, data.height / 2]);
+
+	var path = d3.geo.path()
+	    .projection(projection);
+
+	selection
+		.append('svg').attr({
+			xmlns:'http://www.w3.org/2000/svg',
+			width:data.width,
+			height:data.height
+		}).selectAll('path').data(data.geoJSON.features).enter()
+			.append('path')
+			.attr({
+				'd':path,
+					'id':function(d){ return d.id; }
+				});
 }
