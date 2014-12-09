51119787efd13d46688e87c010a0e1046714e2cc
tomgp <tom.g.pearson@gmail.com>
2014-12-09 15:23:48 +0000

    simplify jsdom stuff

--- app/graphics/svg.js
+++ app/graphics/svg.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var nodeD3 = require('d3');
-var jsdom = require('jsdom');
+var jsdom = require('jsdom').jsdom;
 var decorators = require('./decorators.js');
 var thunkify = require('thunkify');
 
@@ -9,35 +9,16 @@ var doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C
 
 function svgGraphic(decoratorName, data, callback){
 	var decorator = decorators[decoratorName];
-	var doc = jsdom.env({
-		html:'<html><body></body></html>',
-		features:{QuerySelector:true},
-		done:function(errors, window){
-			var type = typeof decorator;
-			if(type != undefined && type == 'function'){
-				window.d3 = nodeD3;
-				window.d3.select('body').html('');
-				var htmlbody = window.d3.select('body');
-				
-				htmlbody.call( decorator, data );
-
-				if(errors){
-					window.__stopAllTimers();
-					window.close();
-					callback ( null, doctype + '' );			
-				}else{
-					var markup = htmlbody.html()
-					window.__stopAllTimers();
-					window.close();
-					callback ( null, doctype + markup );
-				}
-			}else{
-				callback(null, 'not a graphic');
-			}
-			window.__stopAllTimers();
-			window.close();
-		}
-	});
+	var document = jsdom();
+	var window = document.parentWindow;
+	window.d3 = nodeD3;
+	window.d3.select('body').html('');
+	var htmlbody = window.d3.select('body');
+	htmlbody.call( decorator, data );
+	var markup = htmlbody.html()
+	window.__stopAllTimers();
+	window.close();
+	callback ( null, doctype + markup );
 }
 
 module.exports = thunkify( svgGraphic );
