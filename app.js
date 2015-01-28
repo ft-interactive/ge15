bce361c1aa7ce675f2ca70ef464a66f6f4cc3ee0
Luke Kavanagh <ltkavanagh@gmail.com>
2015-01-28 17:42:11 +0000

    Speed app restart.

--- app.js
+++ app.js
@@ -5,7 +5,4 @@ var server = http.createServer(app.callback());
 var db = require('./server/models');
 var port = process.env.PORT || 3000
 
-co(function *(){
-  var connection = yield db.sequelize.authenticate();
-  server.listen(port);
-})();
+server.listen(port);
