4f4b9dc4add6f3e05d798a938119cac41d4a2b04
tomgp <tom.g.pearson@gmail.com>
2015-02-02 16:58:37 +0000

    temporarily comment out the header stuff errors...

--- client/js/main.js
+++ client/js/main.js
@@ -1,7 +1,10 @@
 'use strict';
 
-require('./header.js');
+//require('./header.js');
 require('fetch');
+
+document.graphics = {};
+document.graphics.slope = require('./slope-chart/index.js');
 
 document.addEventListener('DOMContentLoaded', function() {
   document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
