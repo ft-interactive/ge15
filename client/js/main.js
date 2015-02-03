9c2c2410db8010836d8e5fecd5aae6c54fb265b3
tomgp <tom.g.pearson@gmail.com>
2015-02-03 17:04:30 +0000

    created constituency locator as a module

--- client/js/main.js
+++ client/js/main.js
@@ -5,6 +5,7 @@ require('./header.js');
 
 document.graphics = {};
 document.graphics.slope = require('./slope-chart/index.js');
+document.graphics.constituencyLocator = require('./locator-map/constituency-locator.js');
 
 document.addEventListener('DOMContentLoaded', function() {
   document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
