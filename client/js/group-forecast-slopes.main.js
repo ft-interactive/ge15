72f753a9441eb3a290c5e40f10c625faa4b851ca
tomgp <tom.g.pearson@gmail.com>
2015-02-05 13:37:10 +0000

    neaten indenting

--- client/js/group-forecast-slopes.main.js
+++ client/js/group-forecast-slopes.main.js
@@ -42,26 +42,26 @@ function loaded(){
     });
 
   var SVGSlope = graphics.slope.svg()
-  .width(slopeWidth)
-  .radius(2)
-  .endClass(function(d){
-    return  ' end-point ' +d.data.party;
-  })
-  .startClass(function(d){
-    return d.data.party + ' start-point';
-  })
-  .slopeClass(function(d){
-    return d.data.party + ' slope';
-  })
-  .labelClass(function(d){
-    return d.data.party + ' slope-label';
-  })
-  .label(function(d,i){
-    if( d.data.winner ){
-      return document.data.partyShortNames[d.data.party];
-    }
-  })
-  .scale(slopeScale);
+    .width(slopeWidth)
+    .radius(2)
+    .endClass(function(d){
+      return  ' end-point ' +d.data.party;
+    })
+    .startClass(function(d){
+      return d.data.party + ' start-point';
+    })
+    .slopeClass(function(d){
+      return d.data.party + ' slope';
+    })
+    .labelClass(function(d){
+      return d.data.party + ' slope-label';
+    })
+    .label(function(d,i){
+      if( d.data.winner ){
+        return document.data.partyShortNames[d.data.party];
+      }
+    })
+    .scale(slopeScale);
 
   function status(response) {
     if (response.status >= 200 && response.status < 300) {
@@ -83,11 +83,9 @@ function loaded(){
       window.redrawSlopes();
     });
 
-    function gotData(error, data, map){
-
-      var groups = document.data.processForecastData(data);
-
-      var divs = d3.select('#slope-groups')
+  function gotData(error, data, map){
+    var groups = document.data.processForecastData(data);
+    var divs = d3.select('#slope-groups')
       .selectAll('div.group')
       .data(groups)
       .enter()
@@ -97,97 +95,96 @@ function loaded(){
         'data-o-grid-colspan':12
       });
 
-      divs.append('h2')
+    divs.append('h2')
       .attr('class','article-body__subhead figure__title')
       .text(function(d){return d.headline; });
 
-      var overview = divs.append('div')
+    var overview = divs.append('div')
       .attr({'data-o-grid-colspan':'12 S4 M3 L2'})
       .append('div')
       .attr({'class':'o-grid-row constituency-group__details'});
 
-      overview.call(document.tables.forecastSummary);
-      overview.each(function(g,i){
-        var locator = graphics.constituencyLocator()
-        .map(map)
-        .locations(g.constituencies);
+    overview.call(document.tables.forecastSummary);
+    overview.each(function(g,i){
+      var locator = graphics.constituencyLocator()
+      .map(map)
+      .locations(g.constituencies);
 
-        d3.select(this).call(locator);
-      });
-      divs.call(groupContainer);
-    }
+      d3.select(this).call(locator);
+    });
+    divs.call(groupContainer);
+  }
 
-    function groupContainer(g){
-      g = g.append('div')
+  function groupContainer(g){
+    g = g.append('div')
       .attr({'data-o-grid-colspan':'12 S8 M9 L10'})
       .append('div')
       .attr({'class':'o-grid-row'});
 
 
-      var container = g.selectAll('div.constituency').data(function(d){
+    var container = g.selectAll('div.constituency').data(function(d){
         return d.constituencies;
-      }).enter()
+      })
+      .enter()
       .append('div').attr({
         'class':'constituency-group__slope',
         'data-o-grid-colspan':'4 M3 L2 XL2'
       });
 
-      var divs = container.append('div').attr({
+    var divs = container.append('div').attr({
         'class':'constituency-group__slope-graphic'
       });
 
-      container.append('div')
+    container.append('div')
       .attr({
         'class':'constituency-group__constituency-name'
       }).text(function(d){
         return d.name;
       });
 
-      divs.append('svg').attr({
-        class:'slope-chart',
-        'data-constituency':function(d){ return d.id; },
-        viewBox:'0 0 ' + width + ' ' + height + '',
-        preserveAspectRatio:'xMinYMid meet',
-        width:width,
-        height:height
-      }).append('g').attr({
-        'transform':'translate(' + margin.left + ',' + margin.top + ')'
-      }).each(function(d){
-        d3.select(this).selectAll('g.slope')
-        .data(layout(d.parties).sort(function (a,b){
-          if(a.slopeEnd<b.slopeEnd) return -1;
-            if(a.slopeEnd>b.slopeEnd) return 1;
-              return 0;
-            }))
-            .call(SVGSlope);
-          });
+    divs.append('svg').attr({
+      class:'slope-chart',
+      'data-constituency':function(d){ return d.id; },
+      viewBox:'0 0 ' + width + ' ' + height + '',
+      preserveAspectRatio:'xMinYMid meet',
+      width:width,
+      height:height
+    }).append('g').attr({
+      'transform':'translate(' + margin.left + ',' + margin.top + ')'
+    }).each(function(d){
+      d3.select(this).selectAll('g.slope')
+      .data(layout(d.parties).sort(function (a,b){
+        if(a.slopeEnd<b.slopeEnd) return -1;
+          if(a.slopeEnd>b.slopeEnd) return 1;
+            return 0;
+          }))
+          .call(SVGSlope);
+        });
+  }
 
+  window.redrawSlopes = function(){
+    //Resize the SVG
+    var size = d3.select('.constituency-group__slope-graphic').node().getBoundingClientRect();
+    
+    slopeWidth = size.width - (margin.left + margin.right);
+    slopeHeight = slopeWidth/slopeAspect;
+    slopeScale.range([slopeHeight,0]);
 
-        }
+    SVGSlope
+      .width(slopeWidth)
+      .scale(slopeScale);
 
-        window.redrawSlopes = function(){
-          //Resize the SVG
-          var size = d3.select('.constituency-group__slope-graphic').node().getBoundingClientRect();
+    d3.selectAll('svg.slope-chart').each(function(d,i){
+      var w = slopeWidth + (margin.left+margin.right);
+      var h = slopeHeight + (margin.top+margin.bottom);
 
-          slopeWidth = size.width - (margin.left + margin.right);
-          slopeHeight = slopeWidth/slopeAspect;
-          slopeScale.range([slopeHeight,0]);
-
-          SVGSlope
-            .width(slopeWidth)
-            .scale(slopeScale);
-
-          d3.selectAll('svg.slope-chart').each(function(d,i){
-            var w = slopeWidth + (margin.left+margin.right);
-            var h = slopeHeight + (margin.top+margin.bottom);
-
-            var svg = d3.select(this);
-            svg.selectAll('g.slope').call(SVGSlope.reposition);
-            svg.attr({
-              width: w,
-              height: h,
-              viewBox: '0 0 ' + w + ' ' + h + ''
-            });
-          });
-        };
-      }
+      var svg = d3.select(this);
+      svg.selectAll('g.slope').call(SVGSlope.reposition);
+      svg.attr({
+        width: w,
+        height: h,
+        viewBox: '0 0 ' + w + ' ' + h + ''
+      });
+    });
+  };
+}
