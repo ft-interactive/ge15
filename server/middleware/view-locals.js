db7d9b404671848ab09a166ea36c164550335f90
luke.kavanagh <ltkavanagh@gmail.com>
2015-04-21 12:24:42 +0100

    projection widget as a fragment.

--- server/middleware/view-locals.js
+++ server/middleware/view-locals.js
@@ -3,11 +3,13 @@
 module.exports = function() {
   return function* viewLocals(next) {
     var iframed = !!this.query.iframed;
+    var fragment = !!this.query.fragment;
     this.locals.view = {
       layout: {
         fullWidth: false,
         blankPage: iframed,
         iframed: iframed,
+        fragment: fragment,
         body: {}
       }
     };
