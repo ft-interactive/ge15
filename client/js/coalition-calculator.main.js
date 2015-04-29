ac9498785a039b887c920667b49f0986eb3db66d
Callum Locke <callum.locke@gmail.com>
2015-04-29 16:56:27 +0100

    make room for 5-party coalition label

--- client/js/coalition-calculator.main.js
+++ client/js/coalition-calculator.main.js
@@ -7,7 +7,7 @@ var parties = require('uk-political-parties');
 var debounce = require('lodash/function/debounce');
 
 var sliderMargin = { top:5, left:0, bottom:0, right:0 };
-var coalitionsMargin = {top:1, left:135, bottom:20, right:50};
+var coalitionsMargin = {top:1, left:160, bottom:20, right:50};
 var voteDistribution = [];
 var sliderScale = d3.scale.linear().clamp(true);
 var coalitionScale = d3.scale.linear();
