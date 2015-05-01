a68a550981ab79591215221cac1f944a1fb9a71f
luke.kavanagh <ltkavanagh@gmail.com>
2015-05-01 19:14:04 +0100

    Tests must start the database.

--- test/db/test-update-ge15-PA-seat-results.js
+++ test/db/test-update-ge15-PA-seat-results.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var update = require('../../db/update/ge15-PA-seat-results');
-
+var db = require('../../db/');
+db.start();
 update();
 setInterval(update, 10000);
--- test/db/test-update-national-projections.js
+++ test/db/test-update-national-projections.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var update = require('../../db/update/national-projections');
-
+var db = require('../../db');
+db.start();
 update();
 setInterval(update, 10000);
