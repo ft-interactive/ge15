67a9f0d100200c86db3a12ab64b565dc6145ced4
tomgp <tom.g.pearson@gmail.com>
2015-05-01 13:47:19 +0100

    adding the route  for national overview

--- server/routers/results/index.js
+++ server/routers/results/index.js
@@ -2,13 +2,27 @@
 
 var app = require('../../util/app');
 var _ = require('lodash');
+
+var service = require('../../service');
+
 const viewLocals = require('../../middleware/view-locals');
 const siteNav = require('../../middleware/site-navigation');
 
 
 function* home(next) {
-  var data = {};
+  var data = {
+    overview:yield service.resultNationalOverview()
+  };
   yield this.render('results-index', data);
+  yield next;
+}
+
+function* nationalOverview(next) {
+  var data = {
+      overview:yield service.resultNationalOverview()
+  };
+
+  yield this.render('result-national-overview', data);
   yield next;
 }
 
@@ -22,7 +36,8 @@ function main() {
 
     .router()
 
-    .get('/', home);
+    .get('/', home)
+    .get('/national-overview', nationalOverview);
 }
 
 module.exports = main;
--- server/service/index.js
+++ server/service/index.js
@@ -3,3 +3,4 @@ exports.parties = require('./parties');
 exports.postcode = require('./postcode');
 exports.point = require('./point');
 exports.place = require('./place');
+exports.resultNationalOverview = require('./result-national-overview');
