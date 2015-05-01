e88a28e57d69c4ea233368016a519dd70667188d
luke.kavanagh <ltkavanagh@gmail.com>
2015-05-02 00:40:23 +0100

    Get data in parallel.

--- server/routers/results/index.js
+++ server/routers/results/index.js
@@ -1,23 +1,26 @@
 'use strict';
 
-var app = require('../../util/app');
-var _ = require('lodash');
-
-var service = require('../../service');
-
+const app = require('../../util/app');
+const _ = require('lodash');
+const service = require('../../service');
 const viewLocals = require('../../middleware/view-locals');
 const siteNav = require('../../middleware/site-navigation');
 
+const pages = {
+  home: {
+    title: 'Election results',
+    summary: 'The Conservative/Lib-Dem coalition is likely to hold office.\nExpect new party alliances to boost the majority.'
+  }
+};
 
 function* home(next) {
-  var data = {
-    overview:yield service.resultNationalOverview(),
-    coalitions:yield service.resultNationalCoalitions(),
-    page: {
-      title: 'Election results',
-      summary: 'The Conservative/Lib-Dem coalition is likely to hold office.\nExpect new party alliances to boost the majority.'
-    }
-  };
+  const data = _.zipObject(['overview', 'coalitions'], yield Promise.all([
+    yield service.resultNationalOverview(),
+    yield service.resultNationalCoalitions()
+  ]));
+
+  data.page = pages.home;
+
   yield this.render('results-index', data);
   yield next;
 }
