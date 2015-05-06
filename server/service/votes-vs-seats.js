7975c8a8a5d88e7e4cd11e08869e6a96ddac2089
luke.kavanagh <ltkavanagh@gmail.com>
2015-05-06 15:27:38 +0100

    Wire up votes-vs-seats with data from Loki.

--- server/service/votes-vs-seats.js
+++ server/service/votes-vs-seats.js
@@ -1,89 +1,52 @@
 'use strict';
 
-var _ = require('lodash');
-var ukParties = require('uk-political-parties');
+const _ = require('lodash');
+const db = require('./db');
+
+var nameOverrides = {
+  ld: 'Lib Dems'
+};
+
+var expires;
+var last;
+var age = 1000 * 10;
 
 module.exports = function () {
-  // dummy data, based roughly on electionforecast.co.uk
-  var data = [
-    {
-      party: 'lab',
-      percentVoteWon: 32.6,
-      percentSeatsWon: 283 / 6.5
-    },
-    {
-      party: 'c',
-      percentVoteWon: 34.4,
-      percentSeatsWon: 271 / 6.5
-    },
-    {
-      party: 'ld',
-      percentVoteWon: 12.5,
-      percentSeatsWon: 24 / 6.5
-    },
-    {
-      party: 'green',
-      percentVoteWon: 3.7,
-      percentSeatsWon: 1 / 6.5
-    },
-    {
-      party: 'ukip',
-      percentVoteWon: 10.7,
-      percentSeatsWon: 1 / 6.5
-    },
-    {
-      party: 'snp',
-      percentVoteWon: 3.6,
-      percentSeatsWon: 47 / 6.5
-    },
-    {
-      party: 'dup',
-      percentVoteWon: 0.1,
-      percentSeatsWon: 8 / 6.5
-    },
-    {
-      party: 'sf',
-      percentVoteWon: 0.1,
-      percentSeatsWon: 0 / 6.5
-    },
-    {
-      party: 'pc',
-      percentVoteWon: 0.6,
-      percentSeatsWon: 4 / 6.5
-    },
-    {
-      party: 'alliance',
-      percentVoteWon: 0.1,
-      percentSeatsWon: 0 / 6.5
-    },
-    {
-      party: 'other',
-      percentVoteWon: 1.8,
-      percentSeatsWon: 8 / 6.5
-    },
-    {
-      party: 'sdlp',
-      percentVoteWon: 0.1,
-      percentSeatsWon: 3 / 6.5
-    },
-    {
-      party: 'uup',
-      percentVoteWon: 0.1,
-      percentSeatsWon: 0 / 6.5
-    }
-  ];
 
-  data.forEach(function (d) {
-    d.name = d.party === 'ld' ?
-      'Lib Dem' :
-      ukParties.fullName(d.party);
+  if (last && expires && Date.now() < expires) {
+    return last;
+  }
 
-    d.colour = ukParties.colour(d.party);
+  var parties = db.parties().find().map(function(party) {
+
+    return {
+      id: party.id,
+      name: party.id in nameOverrides ? nameOverrides[party.id] : party.full,
+      percentVoteWon: party.elections.ge15.votes_pc,
+      percentSeatsWon: party.elections.ge15.seats_pc_so_far,
+      colour: party.colour
+    };
+
   });
 
+  var sorted = _.sortByOrder(parties, ['percentVoteWon'], [false]);
+  var other = _.remove(sorted, {id: 'other'})[0];
+  var others = sorted.splice(6, Infinity);
 
-  return Promise.resolve({
+  others.reduce(function(o, p){
+    o.percentVoteWon += p.percentVoteWon;
+    o.percentSeatsWon += p.percentSeatsWon;
+    return o;
+  }, other);
+
+  sorted.push(other);
+
+  expires = Date.now() + age;
+
+  last = Promise.resolve({
     headline: 'Popular vote vs. seats won',
-    parties: _.take(_.sortBy(data, 'percentVoteWon').reverse(), 6),
+    parties: sorted,
   });
+
+  return last;
 };
