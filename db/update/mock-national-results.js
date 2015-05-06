2a0b7871c2082a5ed17609d374596ee39f241259
luke.kavanagh <ltkavanagh@gmail.com>
2015-05-06 01:17:09 +0100

    Mock national results: load a PA StateOfParties (SOP) XML in Loki.

--- db/update/mock-national-results.js
+++ db/update/mock-national-results.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const db = require('../loki');
-const national_projections = require('./national-projections');
-const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+const PA = require('../model/pa');
+const create_file_descriptor = require('./ge15-PA-results').create_file_descriptor;
 
 var already_loaded = false;
-
 
 module.exports = function(cb) {
 
@@ -14,32 +14,13 @@ module.exports = function(cb) {
     return;
   }
 
-  national_projections(function(){
-    var parties = db.getCollection('parties');
-    var updatedParties = parties.find().map(function(party) {
-      party.elections.ge15 = _.clone(party.elections.ge15_projection, true);
-      delete party.elections.ge15.seats_lo;
-      delete party.elections.ge15.seats_hi;
-      // TODO: add mock data for
+  already_loaded = true;
+  var filename = 'Test_General_Election_SOP_650.xml';
+  var xml = fs.readFileSync(path.resolve(__dirname, '../source/' + filename)).toString();
+  var descriptor = create_file_descriptor({name: filename, data: 'Thu May 07 2015 22:00:00 GMT+0100 (BST)'});
 
-      // votes
-      // votes_pc
-      // seats_gain
-      // seats_loss
-
-      // apply hard coded numbers seat and seats_pc over the top
-      // of the projections
-
-      // TODO: -1 from conservatives if speaker is a Conservative and his seat has been called.
-      return party;
-    });
-    var sinnfein = parties.findOne({id: 'sf'});
-    sinnfein.elections.ge15.seats = 5;
-    sinnfein.elections.ge15.seats_pc = 5 / 6.5;
-    var other = parties.findOne({id: 'other'});
-    other.elections.ge15.seats -= 5;
-    other.elections.ge15.seats_pc -= 5 / 5.6;
-    parties.update(updatedParties);
+  PA.update_party_results(descriptor, xml, function(){
     cb();
   });
+
 };
