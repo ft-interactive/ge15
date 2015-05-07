8cfc6c38f13dabd3b0371810381d73bf112dcef1
tomgp <tom.g.pearson@gmail.com>
2015-05-07 10:37:54 +0100

    national results data

--- server/service/result-national-overview.js
+++ server/service/result-national-overview.js
@@ -1,26 +1,57 @@
 'use strict';
 
-module.exports = function(){
+const db = require('./db');
 
-  // var err = new Error('result national overview - reject')
-  // err.status = 500;
-  // return Promise.reject( err );
+var major = ['c','lab','snp','ld','dup','pc','sdlp','ukip','green'];
+
+module.exports = function(){
+  var allParties = db.parties().find().map(simplify);
+
+  var parties = allParties
+    .filter(majorParties)
+    .sort(bySeats);
+
+  var other = allParties.reduce(otherParties,{
+    party:'other' , seats:0 , seats_last:0, seats_change:0
+  });
+
+  parties.push(other);
 
   return Promise.resolve(
   {
-    parties:[
-      {party:"c", seats_change:-26, seats:280, seats_last:280+26},
-      {party:"lab", seats_change:10, seats:268, seats_last:268-10},
-      {party:"snp", seats_change:43, seats:49, seats_last:49-43},
-      {party:"ld", seats_change:-30, seats:27, seats_last:27+30},
-      {party:"dup", seats_change:0, seats:8, seats_last:8},
-      {party:"pc", seats_change:1, seats:4, seats_last:4-1},
-      {party:"sdlp", seats_change:0, seats:3, seats_last:3},
-      {party:"ukip", seats_change:2, seats:2, seats_last:2-2},
-      {party:"green", seats_change:0, seats:1, seats_last:1},
-      {party:"other", seats_change:0, seats:8, seats_last:8}
-    ],
-    updated:"2015-04-30T23:03:04.448Z",
+    parties:parties,
+    //updated:"2015-04-30T23:03:04.448Z",
     source:"PA"
   });
 };
+
+function simplify(d){
+  var o = {
+    party:d.id,
+    seats_last:d.elections.last.seats,
+    seats:d.elections.ge15.seats,
+    seats_change:d.elections.ge15.seats_change
+  };
+  return o;
+}
+
+function bySeats(a,b){
+  return b.seats - a.seats;
+}
+
+function majorParties(d){
+  return major.indexOf(d.party) > -1;
+}
+
+function otherParties(previous, current, index, array){
+  if(major.indexOf(current.party) > -1){ //don't include in the sum
+    return previous;
+  }
+
+  return {
+    party:previous.party,
+    seats:previous.seats + current.seats,
+    seats_last:previous.seats_last + current.seats_last,
+    seats_change:previous.seats_change + current.seats_change
+  };
+}
