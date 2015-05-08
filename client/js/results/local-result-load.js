71950fe0e22782802b3dd0cb49c4de771ee314b7
luke.kavanagh <ltkavanagh@gmail.com>
2015-05-08 12:08:54 +0100

    Winner MP name legibility.

--- client/js/results/local-result-load.js
+++ client/js/results/local-result-load.js
@@ -84,7 +84,7 @@ module.exports = function(){
 
     //add extra info
     html += '<div class="figure__footer"><ul class="seat-search-results__detail-container">';
-    if(seat.elections.ge15.winner !== undefined) html += '<li class="seat-search-results__detail"><b>Winner:</b> ' + seat.elections.ge15.winner.person + ' &ndash; ' + ukParties.fullName(seat.elections.ge15.winner.party) + '</li>';
+    if(seat.elections.ge15.winner !== undefined) html += '<li class="seat-search-results__detail seat-search-results__elected"><b>Winner:</b> <span>' + seat.elections.ge15.winner.person + ' &ndash; ' + ukParties.fullName(seat.elections.ge15.winner.party) + '</span></li>';
     if(seat.elections.ge15.turnout_pc) html += '<li class="seat-search-results__detail"><b>Turnout:</b> ' +seat.elections.ge15.turnout_pc + '% ('+change(seat.elections.ge15.turnout_pc_change)+')</li>';
     if(seat.elections.ge15.winner.majority) html += '<li class="seat-search-results__detail"><b>Majority:</b> ' +seat.elections.ge15.winner.majority_pc + '%</li>';
     html+='</ul></div>';
