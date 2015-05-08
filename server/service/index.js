8e3d12cd9317c8566a21c5e5ad6faface5429a20
Callum Locke <callum.locke@gmail.com>
2015-05-08 02:06:18 +0100

    seat result fragment (unstyled)

--- server/service/index.js
+++ server/service/index.js
@@ -13,6 +13,7 @@ exports.resultNationalSlopes = require('./result-national-slopes');
 exports.votesVsSeats = require('./votes-vs-seats');
 exports.stateOfPlay = require('./state-of-play');
 exports.seatResult = require('./seat-result');
+exports.seatResultFragment = require('./seat-result-fragment');
 exports.neighbours = require('./neighbours');
 exports.seat_select = require('./seat-select');
 exports.seats_digest = require('./seats-digest');
